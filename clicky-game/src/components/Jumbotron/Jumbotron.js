c4fa117dffe4ca8ecb51e20920f3b7faa999eebb
GTrapp16 <GTrapp16@gmail.com>
2018-06-18 18:24:26 -0700

    added some styling

--- clicky-game/src/components/Jumbotron/Jumbotron.js
+++ clicky-game/src/components/Jumbotron/Jumbotron.js
@@ -5,7 +5,7 @@ const Jumbotron = props => (
     <div className="jumbotron jumbotron-fluid">
         <div className="container">
             <div className="dark-box mx-auto">
-                <h1 className="display-4 text-center">Clicky SmashBros</h1>
+                <h1 className="display-4 text-center">ClickBros</h1>
                 <p className="lead text-center">Click a bro to earn points but don't click the same bro twice.</p>
                 <h3 className="text-center">Score: {props.score} | Top Score: {props.topScore}</h3>
                 <h5 className="text-center">{props.message}</h5>
